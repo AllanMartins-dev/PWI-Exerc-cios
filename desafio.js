40a83e5aaadb794242fe4d1d0f8b93c5d193d1f2
AllanMartins-dev <allan.oliveira13@aluno.cps.sp.gov.br>
2026-08-28 16:35:38 -0300

    Delete desafio.js

--- desafio.js
+++
@@ -1,9 +0,0 @@
-let input = require('readline-sync');
-let n1 = input.questionInt("Digite Um numero, com base nele, a resposta será se ele e par ou impar: ");
-let result = n1 % 2;
-
-if(result == 0) {
-    console.log("par");
-} else {
-    console.log("Impar");
-}
